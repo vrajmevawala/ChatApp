54cceafe9b009e2fe3537d732cc0acd947499fb8
vraj <vrajmevawala0007@gmail.com>
2025-07-03 01:11:20 +0530

    update static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,9 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    const distPath = path.resolve(__dirname, '../frontend/dist');
+    app.use(express.static(distPath));
     app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(distPath, 'index.html'));
     })
 }
 
